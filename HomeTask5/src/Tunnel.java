78a893a0602bc11c661a353db49b70c2a6d8dc32
Vasily Kulikov <zel.slonik17@gmail.com>
2020-05-08 21:23:46 +0300

    Пятое домашнее задание. Ошибка в классе Car

---
+++ HomeTask5/src/Tunnel.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.Semaphore;
+
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+    @Override
+    public void go(Car c) {
+        try {
+            Semaphore smp = new Semaphore(2);
+            try {
+                smp.acquire();
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                Thread.sleep(500);
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                smp.release();
+                System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
